fbc9842903545cd56e027bf264a91eabc935bce0
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-06-21 22:26:36 +0100

    added forecast functionality to the app

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function updateDate(){
     let formattedDate = today.toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric', year: 'numeric' });
     date.innerHTML = formattedDate;
 }
+
 function updateWeather(response){
     let temperature = document.querySelector('#temperature');
     console.log(response);
@@ -31,9 +32,37 @@ function handleSearch(event){
     let searchInput = document.getElementById('search-input');
     let searchValue = searchInput.value;
     searchCity(searchValue);
+    getForecast(searchValue);
+}
+
+function displayForecast(response) {
+    let forecastElem = document.querySelector('#forecast');
+    let forecastHTML = '<div class="row">';
+    response.data.daily.forEach(function(day, index) {
+        if (index < 5) {
+            forecastHTML += `
+                <div class="col">
+                    <div class="weather-forecast-date">${new Date(day.time * 1000).toLocaleDateString('en-GB', { weekday: 'short' })}</div>
+                    <img src="${day.condition.icon_url}" alt="weather icon" class='weather-forecast-img'>
+                    <div class="weather-forecast-temp d-flex justify-content-between">
+                        <span class="weather-forecast-temp-max"> ${Math.round(day.temperature.maximum)}° </span>
+                        <span class="weather-forecast-temp-min">${Math.round(day.temperature.minimum)}°</span>
+                    </div>
+                </div>
+            `;
+        }
+    });
+    forecastHTML += '</div>';
+    forecastElem.innerHTML = forecastHTML;
+}
+
+function getForecast(city){
+    let apiKey = 'a6t3e024559eaa3385b451317o9c7f98';
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 let searchForm = document.querySelector(".search-form");
 searchForm.addEventListener('submit', handleSearch);
 searchCity("ilorin");
-
+getForecast("ilorin");
